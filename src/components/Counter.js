1171a5e09e7c4e1ee4de643db2f58d5a03421fcd
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-15 03:12:43 -0500

    Added base folder scaffold.

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    
+    render() { 
+        return ( 
+            <div>
+                Counter component
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
